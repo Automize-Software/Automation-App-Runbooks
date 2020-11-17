75951e0207dd0473d272d26ef7ad93f080852744
Lasse Kjelstrup-Hoop <lasse@kjelstrup-hoop.dk>
2020-11-17 09:58:35 +0100

    Initial commit

---
+++ Microsoft Active Directory/Create-User.ps1
@@ -0,0 +1,70 @@
+param (
+  [Parameter(Mandatory=$true)]
+		[string] $username,
+		
+	[Parameter(Mandatory=$true)]
+		[string] $password,
+		
+	[Parameter(Mandatory=$true)]
+		[string] $firstname,
+			
+	[Parameter(Mandatory=$true)]
+		[string] $lastname,
+			
+	[Parameter(Mandatory=$false)]
+		[string] $city,
+			
+	[Parameter(Mandatory=$false)]
+		[string] $office,
+
+	[Parameter(Mandatory=$false)]
+		[string] $mobilePhone,
+		
+	[Parameter(Mandatory=$false)]
+		[string] $officePhone
+)
+
+# * Environment variabels * #
+# Set the below to match your environment #
+$domain = "" #Name of the domain to add the user to
+$domainController = "" #IP or FQDN of Domain Controller
+$path = "" #Path to create the user in. Eg. CN=Users,DC=YourDomain,DC=Internal
+$credentials = "" #Name of stored credentials to use for authentication with Domain Controller
+
+
+### Script ###
+try {
+  $metadata = @{
+    startTime = Get-Date
+    username = $username
+    domain = $domain
+  }
+  
+  Write-Verbose "Runbook started - $($metadata.startTime)" -Verbose
+  
+  $displayname = $firstname + " " + $lastname
+  $samAccountName = $username
+  $userPrincipalName = $username + "@" + $domain
+  $userPassword = ConvertTo-SecureString $password -AsPlainText -Force
+  
+  $user = New-ADUser -SamAccountName $samAccountName `
+  	-UserPrincipalName $userPrincipalName `
+  	-DisplayName $displayname `
+  	-Givenname $firstname `
+  	-Name $displayname `
+  	-Surname $lastname `
+  	-ChangePasswordAtLogon:$False `
+  	-Path $path  `
+    -City $city `
+  	-Office $office `
+  	-MobilePhone $mobilePhone `
+  	-OfficePhone $officePhone `
+    -AccountPassword $userPassword `
+    -Enabled:$True
+} catch {
+  Write-Error ("Exception caught at line $($_.InvocationInfo.ScriptLineNumber), $($_.Exception.Message)") -Verbose
+  throw
+} finally {
+  Write-Verbose "Runbook has completed. Total runtime $((([DateTime]::Now) - $($metadata.startTime)).TotalSeconds) Seconds" -Verbose
+  Write-Output $out | ConvertTo-Json
+}
