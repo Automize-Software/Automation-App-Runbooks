500476f09530fab1410fcec97c9da602d1886d99
Lasse Kjelstrup-Hoop <lasse@kjelstrup-hoop.dk>
2020-12-17 08:33:17 +0100

    Add Jira Issue Comment

--- Atlassian Jira Software/New-Issue-Comment.ps1
+++ Atlassian Jira Software/New-Issue-Comment.ps1
@@ -1,23 +1,20 @@
 param (
     [Parameter(Mandatory=$true)]
-    [string] $summaryText,
+		[string] $issueId,
 		
     [Parameter(Mandatory=$true)]
-    [string] $descriptionText
+        [string] $commentText
 )
 
 # * Environment variabels * #
 # Set the below to match your environment #
-$reporterName = "" #Name of the user that should be set as the reporter on the issue in Jira
-$projectKey = "" #Key of the project to which the issue should be added. Ex: AA
-$issueTypeId = "" #Id of the issue type. Ex: 10004
-$apiURL = "" #API endpoint. Ex: https://automize.atlassian.net/rest/api/3
+$apiURL = "https://automize.atlassian.net/rest/api/3" #API endpoint. Ex: https://automize.atlassian.net/rest/api/3/
 $credName = "" #Name of API Token login credetinals. Ex: Jira-Admin
 
 ### Script ###
 try{
     $cred = Get-AutomationPSCredential -Name $credName
-    $url = "$apiURL/issue/"
+    $url = "$apiURL/issue/$issueId/comment"
     $bytes = [System.Text.Encoding]::UTF8.GetBytes(
         ('{0}:{1}' -f $cred.UserName, $cred.GetNetworkCredential().Password)
     )
@@ -27,45 +24,28 @@ try{
         'Accept' = 'application/json'
         'Content-Type' = 'application/json'
     }
-    $project = @{
-        'key' = $projectKey
-    }
-    $reporter = @{
-        'name' = $reporterName
-    }
-    $issuetype = @{
-        'id' = $issueTypeId
-    }
-    [hashtable]$description = @{}
-    $description.version = 1
-    $description.type = "doc"
-    $description.content = @()
+    [hashtable]$comment = @{}
+    $comment.version = 1
+    $comment.type = "doc"
+    $comment.content = @()
     [hashtable]$content = @{}
     $content.type = "paragraph"
     $content.content= @()
     $paragraph = @{
         'type' = 'text'
-        'text' = $descriptionText
+        'text' = $commentText
     }
     $content.content += $paragraph
-    $description.content += $content
+    $comment.content += $content
     [hashtable]$jiraInput = @{}
-    [hashtable]$jiraInput.update = @{
-    }
-    [hashtable]$jiraInput.fields = @{
-        'project' = $project
-        'summary' = $summaryText
-        'description' = $description
-        'issuetype' = $issuetype
-        'reporter' = $reporter
-    }
+    $jiraInput.body = $comment
     $json = $jiraInput | ConvertTo-Json -Depth 6
     $json = $json -replace '[\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF]/g', ''
     $jsonEncoded = [System.Text.Encoding]::UTF8.GetBytes($json)
-    $issue = Invoke-RestMethod -Method "POST" -Uri $url -Headers $Headers -Body $jsonEncoded | ConvertTo-Json
+    $commentResponse = Invoke-RestMethod -Method "POST" -Uri $url -Headers $Headers -Body $jsonEncoded | ConvertTo-Json
 } catch {
     Write-Error -Message $_.Exception
     throw $_.Exception
 } finally {
-    Write-Output $issue
+    Write-Output $commentResponse
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AzureRunbooks
 This repository contains open source runbook templates that can be used with Automation App for ServiceNow available from store.service-now.com
+The Automation App makes it possible to use Microsoft Azure Automation directly from ServiceNow.
+For additional information or support please contact info@automize-software.com
